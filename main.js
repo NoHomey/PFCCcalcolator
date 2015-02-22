7d675d9bc5cc8645de0bd1e61c0dac9383bd582f
Ivo Stratev <ivo_stratev@abv.bg>
2015-02-22 09:45:34 +0000

    Support for multiple metrics

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ DataBase = {
 function LockAll(a) {
     document.getElementById("Select1").disabled = a;
     document.getElementById("Select2").disabled = a;
-    document.getElementById("Select3").disabled = a
+    document.getElementById("Select3").disabled = a;
 }
 
 function ExpandAll(a) {
@@ -43,17 +43,17 @@ function ExpandAll(a) {
         document.getElementById("opt").value = "UnList Options";
         document.getElementById("Select1").size = document.getElementById("Select1").length;
         document.getElementById("Select2").size = document.getElementById("Select2").length;
-        document.getElementById("Select3").size = document.getElementById("Select3").length
+        document.getElementById("Select3").size = document.getElementById("Select3").length;
     } else {
-        document.getElementById("opt").value = "List Options"
+        document.getElementById("opt").value = "List Options";
     }
 }
 
 function onEachClick() {
     if (document.getElementById("opt").value === "List Options") {
-        ExpandAll("UnList Options")
+        ExpandAll("UnList Options");
     } else {
-        ExpandAll("List Options")
+        ExpandAll("List Options");
     }
 }
 
@@ -65,7 +65,7 @@ function getKey() {
     a = a + b.options[b.selectedIndex].text;
     var b = document.getElementById("Select3");
     a = a + b.options[b.selectedIndex].text;
-    return a
+    return a;
 }
 
 function printResult() {
@@ -76,22 +76,30 @@ function printResult() {
     document.getElementById("outP").innerHTML = a + protn;
     document.getElementById("outF").innerHTML = b + fat;
     document.getElementById("outC").innerHTML = d + carbs;
-    document.getElementById("outCal").innerHTML = c + cals
+    document.getElementById("outCal").innerHTML = c + cals;
 }
 
-function addIt() {
+function addToStack() {
     var Qua = document.getElementById("Select4").value;
+    var kg = 1;
+    var oz = 1;
+    var lb = 1;
+    var e = document.getElementById("Select5");
+    if (e.options[e.selectedIndex].text === "Kilogram (kg)") {kg = 1000;}
+    if (e.options[e.selectedIndex].text === "Pound (lb)") {lb = 0.0022046;}
+    if (e.options[e.selectedIndex].text === "Ounce (oz)") {oz = 0.035274;}
+    Qua = Qua * ((1 * kg) * (1 * oz) * (1 * lb)); 
     var a = getKey();
     protn += Qua * (DataBase[a].Protein / 100);
     fat += Qua * (DataBase[a].Fat / 100);
     carbs += Qua * (DataBase[a].Carbs / 100);
     cals += Qua * (DataBase[a].Cals / 100);
-    printResult()
+    printResult();
 }
 
-function refToAddIt(e) {
+function refToAddToStack(e) {
     if (e.keyCode === 13) {
-        addIt();
+        addToStack();
         return false;
     }
 }
@@ -100,7 +108,7 @@ function clearAllOpt() {
     var a = document.getElementById("Select2");
     var b;
     for (b in a.options) {
-        a.options[b] = null
+        a.options[b] = null;
     }
 }
 
@@ -117,6 +125,6 @@ function dysplayChoices() {
     for (var b in SmartChoice[c]) {
         d = document.createElement("option");
         d.text = SmartChoice[c][b];
-        a.add(d)
+        a.add(d);
     }
 };
