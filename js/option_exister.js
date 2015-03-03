b913adbba3048c79002529ad1290f039e005efcc
NoHomey <ivo_stratev@abv.bg>
2015-03-03 13:37:11 +0200

    Refactoring js/option_exister.js

--- js/option_exister.js
+++ js/option_exister.js
@@ -1,11 +1,11 @@
-function OptionExister () {
-	this.OptionExist = function (Element, Option) {
-		var Exist = false;
-		for( var index in Element.options) {
-			if (Element.options[index] === Option) {
-				Exist = true;
-			}
-		} 
-		return Exist;
-	}
+function OptionExister() {
+    this.OptionExist = function (Element, Option) {
+        var Exist = false;
+        for (var Index in Element.options) {
+            if (Element.options[Index] === Option) {
+                Exist = true;
+            }
+        }
+        return Exist;
+    }
 }
