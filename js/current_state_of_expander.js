7528b582a5295275bc904dec7e2c2474496c2eee
NoHomey <ivo_stratev@abv.bg>
2015-03-03 11:10:15 +0200

    refactoring js/current_state_of_expander.js

--- js/current_state_of_expander.js
+++ js/current_state_of_expander.js
@@ -1,12 +1,14 @@
 function CurrentStateOfExpander() {
     this.CurrentState = function (State) {
-        document.getElementById("Select1").size = document.getElementById("Select2").size = document.getElementById("Select3").size = document.getElementById("Select5").size = 1;
+        var ArrayOfIds = ["Select1", "Select2", "Select3", "Select5"];
+        for (var Index in ArrayOfIds) {
+            document.getElementById(ArrayOfIds[Index]).size = 1;
+        }
         if (State === "UnList Options") {
             document.getElementById("InterfaceExpander").value = "UnList Options";
-            document.getElementById("Select1").size = document.getElementById("Select1").length;
-            document.getElementById("Select2").size = document.getElementById("Select2").length;
-            document.getElementById("Select3").size = document.getElementById("Select3").length;
-            document.getElementById("Select5").size = document.getElementById("Select5").length;
+            for (var Index in ArrayOfIds) {
+                document.getElementById(ArrayOfIds[Index]).size = document.getElementById(ArrayOfIds[Index]).length;
+            }
         } else {
             document.getElementById("InterfaceExpander").value = "List Options";
         }
