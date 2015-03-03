0097201b6d3cdf5156d61128347877b94fced3fc
NoHomey <ivo_stratev@abv.bg>
2015-03-03 11:26:12 +0200

    refactoring js/element_remover.js

--- js/element_remover.js
+++ js/element_remover.js
@@ -1,12 +1,12 @@
 function ElementRemover() {
-	this.Remove = function(WhichOne) {
-		var HistoryElement = document.getElementById("Number" + WhichOne);
-		if (HistoryElement !== null) {
-			HistoryElement.parentNode.removeChild(HistoryElement);
-			HistoryElement = document.getElementById(WhichOne);
-			HistoryElement.parentNode.removeChild(HistoryElement);
-			HistoryElement = document.getElementById(WhichOne);
-			HistoryElement.parentNode.removeChild(HistoryElement);
-		}
-	}
+    this.Remove = function (WhichOne) {
+        var HistoryElement = document.getElementById("Number" + WhichOne);
+        if (HistoryElement !== null) {
+            HistoryElement.parentNode.removeChild(HistoryElement);
+            for (var Repeat = 0; Repeat < 2; Repeat++) {
+                HistoryElement = document.getElementById(WhichOne);
+                HistoryElement.parentNode.removeChild(HistoryElement);
+            }
+        }
+    }
 }
