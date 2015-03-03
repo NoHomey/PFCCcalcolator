4d79cfa8e0f022d8e77745deebbe8b1698e60601
NoHomey <ivo_stratev@abv.bg>
2015-03-03 13:21:50 +0200

    Refactoring js/history_remover.js

--- js/history_remover.js
+++ js/history_remover.js
@@ -1,7 +1,7 @@
 function HistoryRemover() {
-	this.Remove = function(WhichOne) {
-		OldQuantity = document.getElementById("Number" + WhichOne).value.split(' ')[3];
-		InstanceOfElementRemover.Remove(WhichOne);
-		InstanceOfAdderReferencer.ReferenceToAdder(-1);
-	}
+    this.Remove = function (WhichOne) {
+        OldQuantity = document.getElementById("Number" + WhichOne).value.split(' ')[3];
+        InstanceOfElementRemover.Remove(WhichOne);
+        InstanceOfAdderReferencer.ReferenceToAdder(-1);
+    }
 }
