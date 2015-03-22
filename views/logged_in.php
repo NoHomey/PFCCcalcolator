d0360cfb9f44173be787ae60572566984a956efd
NoHomey <ivo_stratev@abv.bg>
2015-03-22 13:47:00 +0200

    adding cool login

---
+++ views/logged_in.php
@@ -0,0 +1,6 @@
+<!-- if you need user information, just put them into the $_SESSION variable and output them here -->
+<?php echo $_SESSION['user_name']; ?>
+
+
+<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
+<a href="index.php?logout">Logout</a>
