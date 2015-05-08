eb2490b2398a38fb29117a3d43a9eacd3ee032ae
Ivo Stratev <ivo_stratev@abv.bg>
2015-05-08 16:07:17 +0000

    aa

--- vmware.c
+++ vmware.c
@@ -8,7 +8,7 @@ struct MemoryStruct {
   size_t size;
 };
  
-inline char* p_cpy(char* dst, char* src, size_t len) {
+inline void p_cpy(char* dst, char* src, size_t len) {
   size_t i;
   for (i=0; i<len; i++) dst[i] = src[i];
 }
@@ -27,7 +27,6 @@ inline size_t str_cpy(char* dst, char* src) {
     i++;
     if(i==4) break;
   }
-
   return i;
 }
 
@@ -44,11 +43,20 @@ inline size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
   return realsize;
 }
 
-inline bool compare(char* ptr1, char* ptr2) {
+inline bool equal(char* ptr1, char* ptr2) {
   return ((ptr1[0] == ptr2[0]) && (ptr1[1] == ptr2[1]) && (ptr1[2] == ptr2[2]) && (ptr1[3] == ptr2[3]));
 }
 
-inline void init (char* tar) {
+inline bool not_in(char* search, char** where, size_t len) {
+  size_t i;
+  for(i = 0; i < len; i++) {
+    if(equal(search, where[i]) == false) return true;
+  }
+  if(i == 0) return true;
+  else return false;
+}
+
+inline size_t init (char* tar, char* chil, char* per) {
   tar[0] = 't';
   tar[1] = 'r';
   tar[2] = 'a';
@@ -60,6 +68,15 @@ inline void init (char* tar) {
   tar[8] = 'r';
   tar[9] = 'y';
   tar[10] = '=';
+  size_t pos = str_cpy(tar + 11, chil);
+  pos += 11;
+  tar[pos++] = ' ';
+  return pos + str_cpy(tar + pos, per);
+}
+
+inline size_t inc_traject(char* tar, char* node, size_t pos) {
+  tar[pos++] = ' ';
+  return pos + str_cpy(tar + pos, node);
 }
 
 int main(void) {
@@ -70,7 +87,7 @@ int main(void) {
   get_es.size = 0;  
   curl_global_init(CURL_GLOBAL_ALL);
   curl_handle = curl_easy_init();
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://83.143.146.64:8080/api/sector/1/objects");
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://83.143.146.64:8080/api/sector/9/objects");
   curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
   curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&get_es);
   curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
@@ -78,15 +95,13 @@ int main(void) {
   struct MemoryStruct get_rs; 
   get_rs.memory = malloc(1);  
   get_rs.size = 0; 
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://83.143.146.64:8080/api/sector/1/roots");
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://83.143.146.64:8080/api/sector/9/roots");
   curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&get_rs);
   result = curl_easy_perform(curl_handle);
   int i;
   int k = 0;
   int l = 0;
-  int n;
-  int flag1 = 1;
-  int flag2 = 1;
+  int flag;
   int rs_len = 0;
   int es_len = 0;
   int fs_len = 0;
@@ -95,11 +110,8 @@ int main(void) {
   int ts_len = 0;
   int founds[2000];
   int lens[2000];
-  bool cmp_res;
-  bool cmp_res1;
-  bool cmp_res2;
   char* perent = calloc(1, 4);
-  char* traject = calloc(1000, 4);
+  char* traject = calloc(1500, 4);
   char** roots = calloc(1000, 4);
   char** cycle = calloc(1000, 4);
   char** nodes = calloc(1000, 4);
@@ -115,7 +127,7 @@ int main(void) {
      edges[i][0] = calloc(4, 4); 
      edges[i][1] = calloc(4, 4);
    }
-   for(i = 0;i<800;i++) trajecs[i] = calloc(1000, 4); 
+   for(i = 0;i<800;i++) trajecs[i] = calloc(1500, 4); 
    i = k = 0;
   while(i < get_rs.size - 1) {
    if(get_rs.memory[i] == '\n') {
@@ -148,8 +160,7 @@ int main(void) {
   /*for(i = 0;i<es_len;i++) printf("s%d: %s %s\n",i, edges[i][0], edges[i][1]);
   sleep(10);
   for(i = 0;i<rs_len;i++) printf("r%d: %s\n",i, roots[i]);
-     sleep(10);
-ARSING RESPONSES AND GETING ROOTS AND edges AS ARRAYS!!!!!!!!!!!!!!!*/
+     sleep(10);*/
   memcpy(cycle,roots, rs_len);
   ce_len = rs_len;
   fs_len = 1;
@@ -157,7 +168,7 @@ ARSING RESPONSES AND GETING ROOTS AND edges AS ARRAYS!!!!!!!!!!!!!!!*/
     fs_len = 0;
     for(i = 0; i < es_len;i++) {
       for(k = 0; k < ce_len;k++) {
-        if(compare(edges[i][0], cycle[k])) {
+        if(equal(edges[i][0], cycle[k])) {
           founds[fs_len++] = i;
           break;
         }
@@ -167,8 +178,7 @@ ARSING RESPONSES AND GETING ROOTS AND edges AS ARRAYS!!!!!!!!!!!!!!!*/
     for(i=0; i < fs_len;i++) {
        my_cpy(cycle[ce_len], edges[founds[i]][1]);
        ce_len++;
-       for(l = 0; l < rs_len;l++) cmp_res = compare(roots[l], edges[founds[i]][1]);
-       if (!cmp_res) {
+       if (not_in(edges[founds[i]][1], roots, rs_len)) {
          my_cpy(roots[rs_len], edges[founds[i]][1]);
          rs_len++;
        }
@@ -179,10 +189,8 @@ ARSING RESPONSES AND GETING ROOTS AND edges AS ARRAYS!!!!!!!!!!!!!!!*/
 //Purviqt etap RDY!
   for(i = 0;i<es_len;i++) {
     for(k = 0; k < rs_len; k++) {
-       if(compare(edges[i][1], roots[k])) {
-          cmp_res = 0;
-          for(l =0; l < ns_len;l++) cmp_res = compare(edges[i][0], nodes[l]);
-          if(!cmp_res) {
+       if(equal(edges[i][1], roots[k])) {
+          if(not_in(edges[i][0], nodes, ns_len)) {
             my_cpy(nodes[ns_len], edges[i][0]);
             ns_len++;
             founds[fs_len++] = i;
@@ -193,56 +201,48 @@ ARSING RESPONSES AND GETING ROOTS AND edges AS ARRAYS!!!!!!!!!!!!!!!*/
   }
   for(i = 0;i < fs_len;i++) edges[founds[i]][0][0] = edges[founds[i]][1][0] = '\0';
   ce_len = 0;
-  /*while(flag1) {
-    flag1 = 0;
+  while(1) {
+    flag = 0;
     for(i = 0;i < es_len;i++) {
-       cmp_res1  = 0;
-       cmp_res2 = 0;
-       for(k = 0; k < ce_len;k++) cmp_res1 = compare(edges[i][0], cycle[k]);
-       for(k = 0; k < ce_len;k++) cmp_res2 = compare(edges[i][1], cycle[k]);
-       if((!compare(edges[i][0], edges[i][1])) && (!cmp_res1) && (!cmp_res2) && (edges[i][0][0] != '\0')) {
+       if((!equal(edges[i][0], edges[i][1])) && (not_in(edges[i][0], cycle, ce_len)) && (not_in(edges[i][1], cycle, ce_len)) && (edges[i][0][0] != '\0')) {
          my_cpy(perent, edges[i][1]);
-         init(traject);
-         n = str_cpy(traject + 11,edges[i][0]); 
-         l += n;
-         traject[l++] = ' ';
-         n = str_cpy(traject + l,perent);
-         l += n;
+         l = init(traject, edges[i][0], perent);
          my_cpy(cycle[ce_len], edges[i][0]);
          ce_len++;
          my_cpy(cycle[ce_len], perent);
          ce_len++;
          fs_len = 0;
          founds[fs_len++] = i;
-         flag1 = 1;
-         //printf("\n %d %s\n",ts_len, traject);
+         flag = 1;
          break;
        }
     }
-    flag2 = 1;
-    while(flag2) {
-      flag2 = 0;
+    if(!flag) break;
+    while(1) {
+      flag = 0;
       for(i = 0;i < es_len;i++) {
-        cmp_res = 0;
-        for(k = 0; k < ce_len;k++) cmp_res = compare(edges[i][1], cycle[k]);
-        if((compare(perent,edges[i][0])) && (!compare(edges[i][0],edges[i][1])) && (!cmp_res)) {
+        if((equal(perent,edges[i][0])) && (!equal(edges[i][0],edges[i][1])) && (not_in(edges[i][1], cycle, ce_len))) {
           my_cpy(perent, edges[i][1]);
-          n = str_cpy(traject + l, perent);
-          l += n;
-          traject[l++] = ' ';
+          k = inc_traject(traject, perent, l);
+          l = k;
           my_cpy(cycle[ce_len], perent);
           ce_len++;
           founds[fs_len++] = i;
-          flag2 = 1;
+          flag = 1;
           break;
         }
       }
+    if(!flag) break;
     }
-   // printf("\n %d %s\n",ts_len, traject);
+    printf("\n %d %s\n",ts_len, traject);
+    printf("\n %d %d\n", l, strlen(traject));
+    sleep(1);
     p_cpy(trajecs[ts_len], traject, l);
+    free(traject);
+    char* traject = calloc(1500, 4); 
     lens[ts_len++] = l;
     for(i = 0;i < fs_len;i++) edges[founds[i]][0][0] = edges[founds[i]][1][0] = '\0';
-  }*/
+    }
 
 
 
