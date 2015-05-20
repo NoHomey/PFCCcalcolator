2450870cbeb718345d3ccadf58a0095cda579ec6
NoHomey <ivo_stratev@abv.bg>
2015-05-20 13:48:17 +0300

    TryTI

---
+++ calcolator_panels/user.php
@@ -0,0 +1,3 @@
+<form class="Update" id="user" name="user">
+    <span class="User"><?php echo $_SESSION['user_name']; ?></span> <a class="Remove" href="index.php?logout" id="logout">Logout</a>
+</form>
