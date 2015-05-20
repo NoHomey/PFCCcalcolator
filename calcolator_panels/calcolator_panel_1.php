c1987532ca10df2697a5be1e5881e29456afdb0a
NoHomey <ivo_stratev@abv.bg>
2015-05-20 13:52:03 +0300

    Finaly

--- calcolator_panels/calcolator_panel_1.php
+++ calcolator_panels/calcolator_panel_1.php
@@ -1,7 +1,3 @@
-<form class="Update" id="user" name="user">
-    <span class="User"><?php echo $_SESSION['user_name']; ?></span> <a class="Remove" href="index.php?logout" id="logout">Logout</a>
-</form>
-
 <div class="Out" id="OutPut">
     <strong id="outP">Total Protein: 0.0000</strong>
     <br>
--- calcolator_panels/calcolator_panel_4.php
+++ calcolator_panels/calcolator_panel_4.php
@@ -1,6 +1,6 @@
 <form action="main.php" id="History" method="post" name="file_content">
-    <input class="Update" name="file_save_it" onclick="InstanceOfRequester.SendRequest()" type="button" value="Save current state of the Calcolation">
-    <input autocomplete="off" class="Remove" name="file_delete" type="submit" value="Delete this Calcolation">
+    <input class="Update" id="Save" name="file_save_it" onclick="InstanceOfRequester.SendRequest()" type="button" value="Save current state of the Calcolation">
+    <input class="Remove" name="file_delete" type="submit" value="Delete this Calcolation">
     <input class="LastElement" id="Number0" type="button" value="end">
     <br></br>
 </form>
